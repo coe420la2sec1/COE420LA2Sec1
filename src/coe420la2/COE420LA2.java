27f9bf058ab202d4a189e706cb21fb67c3f62fc0
Noor Awali <g00057978@aus.edu>
2019-02-10 14:39:02 +0400

    Inital Version

---
+++ src/coe420la2/COE420LA2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420la2;
+
+/**
+ *
+ * @author g00057978
+ */
+public class COE420LA2 
+{
+public static void addFeatureA()
+{
+    System.out.println("Feature Added");
+    
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        addFeatureA();
+        
+    }
+    
+}
